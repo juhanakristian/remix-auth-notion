468650ce603fd21f35a5db33aab946fe92d7f7e2
Juhana Jauhiainen <juhana.jauhiainen@gmail.com>
2022-02-13 15:21:29 +0200

    fix: use camelCase for profile attributes

--- src/index.ts
+++ src/index.ts
@@ -27,7 +27,7 @@ export interface NotionProfile {
   id?: string;
   name?: string;
   email?: string;
-  avatar_url?: string;
+  avatarUrl?: string;
 }
 
 export interface NotionExtraParams
@@ -98,7 +98,7 @@ export class NotionStrategy<User> extends Strategy<User, NotionVerifyParams> {
       type: "user",
       id: user.id,
       name: user.name,
-      avatar_url: user.avatar_url,
+      avatarUrl: user.avatar_url,
       email: user.person?.email,
     };
   }
